3216464d782af8cca045f71ac25154f105187a09
sid2503 <yaroslav.dvoinyh@gmail.com>
2023-08-18 01:48:00 +0300

    +

--- src/configuration.py
+++ src/configuration.py
@@ -3,6 +3,7 @@
 import logging
 from dataclasses import dataclass
 from os import getenv
+from sqlalchemy.engine import URL
 
 
 @dataclass
@@ -27,6 +28,31 @@ class RedisConfig:
 
 
 @dataclass
+class DatabaseConfig:
+    """Database connection variables."""
+
+    name: str | None = getenv('POSTGRES_DATABASE')
+    user: str | None = getenv('POSTGRES_USER')
+    passwd: str | None = getenv('POSTGRES_PASSWORD', None)
+    port: int = int(getenv('POSTGRES_PORT', 5432))
+    host: str = getenv('POSTGRES_HOST', 'db')
+
+    driver: str = 'asyncpg'
+    database_system: str = 'postgresql'
+
+    def build_connection_str(self) -> str:
+        """This function build a connection string."""
+        return URL.create(
+            drivername=f'{self.database_system}+{self.driver}',
+            username=self.user,
+            database=self.name,
+            password=self.passwd,
+            port=self.port,
+            host=self.host,
+        ).render_as_string(hide_password=False)
+
+
+@dataclass
 class Configuration:
     """All in one configuration's class."""
 
@@ -35,6 +61,7 @@ class Configuration:
 
     bot = BotConfig()
     redis = RedisConfig()
+    db = DatabaseConfig()
 
 
 conf = Configuration()
--- start_bot.py
+++
@@ -1,7 +0,0 @@
-import asyncio
-import logging
-
-from src.bot.__main__ import start_bot
-
-if __name__ == '__main__':
-    asyncio.run(start_bot())
