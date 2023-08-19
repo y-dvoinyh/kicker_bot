af5eb458f44738cf9203d0d0ed691d88193b355c
ya.dvoynih <ya.dvoynih@tensor.ru>
2023-08-19 16:02:22 +0300

    +

--- src/configuration.py
+++ src/configuration.py
@@ -8,13 +8,13 @@ from sqlalchemy.engine import URL
 from dotenv import load_dotenv
 
 
-# TODO удалить
-dotenv_path = '../../.env'
-alembic_dotenv_path = '.env'
-# TODO удалить
-for p_path in (dotenv_path, alembic_dotenv_path):
-    if path.exists(p_path):
-        load_dotenv(p_path)
+# # TODO удалить
+# dotenv_path = '../../.env'
+# alembic_dotenv_path = '.env'
+# # TODO удалить
+# for p_path in (dotenv_path, alembic_dotenv_path):
+#     if path.exists(p_path):
+#         load_dotenv(p_path)
 
 
 @dataclass
